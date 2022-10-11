06bd7764b98fb8d4ee61b1f12e730bd87e202362
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-10 21:47:12 -0300

    adicionando o código da pratica-5.js da semana 1

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,7 @@
+let nome = 'Marcos'
+let email = 'marcosjr_rj@hotmail.com'
+let profissao = 'Analista de Suporte'
+let idade = 34
+let lugarConhecer = 'EUA'
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para o ${lugarConhecer}. Confirma para gente alguns dados? A sua idade é de ${idade}, você é ${profissao} e o seu email é ${email} ? Ficamos no aguardo. Muito obrigado e boa viagem! :D `)
