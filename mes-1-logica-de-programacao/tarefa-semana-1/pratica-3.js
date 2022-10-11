af232ccc5adf951394b2abd6013554b2e1483c1c
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-10 21:30:51 -0300

    adicionando codigo da pratica-3.js da semana 1

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,14 @@
+let nome = 'Marcos'
+let email = 'marcosjr_rj@hotmail.com'
+let profissao = 'Analista de Suporte'
+let idade = 34
+let lugarConhecer = 'EUA'
+
+
+
+
+console.log(typeof(nome) == 'string')
+console.log(typeof(email) == 'string')
+console.log(typeof(profissao) == 'string')
+console.log(typeof(idade) == 'string')
+console.log(typeof(lugarConhecer) == 'string')
