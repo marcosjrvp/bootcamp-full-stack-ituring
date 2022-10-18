0c5213396fa64286adc7d4afec4973aaccd0984b
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-18 18:08:05 -0300

    adicionando código ao pratica-10 da tarefa semana 2

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -0,0 +1,27 @@
+function obterMaior(numero1, numero2, numero3) {
+
+    if (numero1 === numero2 && numero2 === numero3) {
+        console.log(`Os números digitados foram iguais, portando será exibido somente um deles : ${numero1}`)
+    } else if (numero1 > numero2 && numero1 > numero3) {
+        console.log(`O número ${numero1} foi o maior digitado, portanto somente ele será exibido : ${numero1}`)
+    } else if (numero2 > numero1 && numero2 > numero3) {
+        console.log(`O número ${numero2} foi o maior digitado, portanto somente ele será exibido : ${numero2}`)
+    } else if (numero3 > numero1 && numero3 > numero2) {
+        console.log(`O número ${numero3} foi o maior digitado, portanto somente ele será exibido : ${numero3}`)
+    } else if (numero1 > numero2 && numero1 >= numero3) {
+        console.log(`Há 2 números iguais maiores, portanto somente será exibido um deles :  ${numero1}`)
+    } else if (numero2 > numero1 && numero2 >= numero3) {
+        console.log(`Há 2 números iguais maiores, portanto somente será exibido um deles :  ${numero2}`)
+    } else if (numero3 > numero1 && numero3 >= numero2) {
+        console.log(`Há 2 números iguais maiores, portanto somente será exibido um deles :  ${numero3}`)
+    } else if (numero1 >= numero2 && numero1 > numero3) {
+        console.log(`Há 2 números iguais maiores, portanto somente será exibido um deles :  ${numero1}`)
+    } else if (numero2 >= numero1 && numero2 > numero3) {
+        console.log(`Há 2 números iguais maiores, portanto somente será exibido um deles :  ${numero2}`)
+    } else if (numero3 >= numero1 && numero3 > numero2) {
+        console.log(`Há 2 números iguais maiores, portanto somente será exibido um deles :  ${numero3}`)
+
+    }
+}
+
+console.log(obterMaior())
