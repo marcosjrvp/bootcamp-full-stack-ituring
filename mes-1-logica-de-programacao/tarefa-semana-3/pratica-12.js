78be66c453b8f532bc31f45e0720beb1ad7bf3b2
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-22 16:36:37 -0300

    adicionando código ao pratica-12.js da tarefa-semana 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
@@ -0,0 +1,12 @@
+const listaPessoas = ["Anderson Luz", "Fabio Vechia", "Claudia Lopes", "Rafael Alves", "Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"];
+
+function exibirPessoas(listaPessoas){
+
+    
+    console.log('Grupo 1 : '  + listaPessoas[0] +' , ' + listaPessoas[1] );
+    console.log('Grupo 2 : ' + listaPessoas[2] + ' , ' + listaPessoas[3] );
+    console.log('Grupo 3 : ' + listaPessoas[4] + ' , ' + listaPessoas[5] + ' , ' + listaPessoas[6] );
+}
+
+
+exibirPessoas(listaPessoas);
