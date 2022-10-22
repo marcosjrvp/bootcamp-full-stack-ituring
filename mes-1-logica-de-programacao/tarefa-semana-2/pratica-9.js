7819276a7728bc0197d98c95c895e604a107ccf8
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-22 16:25:01 -0300

    atualizando o código da pratica-9.js da tarefa-semana 2

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -1,17 +1,19 @@
-const obterMaior = function(numero1 , numero2){
+const obterMaior = function (numero1, numero2) {
 
-    if(numero1 === numero2){
+    if (numero1 === numero2) {
 
-        resultado = console.log(`Os números digitados foram iguais, portando será exibido somente um deles : ${numero1}`);
-        return resultado;
-    }else if(numero1 > numero2){
+        console.log(`Os números digitados foram iguais, portando será exibido somente um deles : ${numero1}`);
 
-        resultado = console.log(`O número ${numero1} foi o maior digitado, portanto somente ele será exibido : ${numero1}`);
-        return resultado;
-    }else{
-        resultado = console.log(`O número ${numero2} foi o maior digitado, portanto somente ele será exibido : ${numero2}`);
-        return resultado;
+    } else if (numero1 > numero2) {
+
+        console.log(`O número ${numero1} foi o maior digitado, portanto somente ele será exibido : ${numero1}`);
+
+    } else {
+        console.log(`O número ${numero2} foi o maior digitado, portanto somente ele será exibido : ${numero2}`);
+
     }
 }
 
-obterMaior()
+obterMaior(1, 2);
+obterMaior(2, 2);
+obterMaior(4, 2);
