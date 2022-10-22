01672d11147e94f9e1b625a41a121d7bfe24fa7a
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-22 16:35:08 -0300

    adicionando código ao pratica-7.js da tarefa-semana 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
@@ -0,0 +1,15 @@
+const listaNumeros = [100 , -20 , -30 , 10 , -7 , -21 , -5];
+
+
+function calcularSaldoFinal(listaNumeros){
+
+    let saldoFinal = 0;
+
+    for(let i=0; i, i<listaNumeros.length; i++){
+        saldoFinal += listaNumeros[i];        
+    }
+    console.log(`O saldo final da sua conta foi de positivo no valor de R$ ${saldoFinal}`);
+}
+
+calcularSaldoFinal(listaNumeros);
+    
