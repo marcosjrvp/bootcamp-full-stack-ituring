5b30b770f602760b673d4b98a677c89e4d58e5af
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-22 16:35:28 -0300

    adicionando código ao pratica-8.js da tarefa-semana 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -0,0 +1,34 @@
+const listaNumeros = [100 , -20 , -30 , 10 , -7 , -21 , -5];
+
+
+
+function calcularSaldoFinal(listaNumeros){
+
+    let saldoFinal = 0;
+
+    for(let i=0; i, i<listaNumeros.length; i++){
+        saldoFinal += listaNumeros[i];        
+    }
+    console.log(`O saldo final da sua conta foi de positivo no valor de R$ ${saldoFinal}`);
+}
+
+function exibirDepositosERetiradas(listaNumeros){
+
+    let totalDepositos = 0;
+    let totalRetiradas = 0;
+
+    for(let i =0; i < listaNumeros.length; i++ ){
+        if(listaNumeros[i] >= 0){
+            totalDepositos += listaNumeros[i];
+        }else if(listaNumeros[i] < 0){
+            totalRetiradas += listaNumeros[i];
+            
+        }
+    }
+    console.log(`O total de depósitos foram de R$ ${totalDepositos}`);
+    console.log(`O total de retiradas foram de R$ ${totalRetiradas*-1}`);
+}
+
+exibirDepositosERetiradas(listaNumeros);
+calcularSaldoFinal(listaNumeros);
+    
