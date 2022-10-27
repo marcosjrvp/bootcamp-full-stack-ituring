5358410b0cfbe27033c93d9d7d75027d3c35bb4a
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-27 17:24:12 -0300

    adicionando código ao pratica-6.js da tarefa da semana-4

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-6.js
@@ -0,0 +1,14 @@
+let filme = {
+    titulo: 'Estrelas Além do Tempo' ,
+    notaDeAvaliacao: 7.8 ,
+    duracao: 127 ,
+    anoDePublicacao: 2016 ,
+    categoria: 'Drama'
+}
+
+const exibirInformacao = (filme => {
+
+    console.log(`O ${filme.titulo} que estreou em ${filme.anoDePublicacao} e tem a avaliação de ${filme.notaDeAvaliacao} pelo IMDB`);
+} );
+
+exibirInformacao(filme);
