3be3827631a5b9f459d0c8c93115d2a761a61b52
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-29 20:12:16 -0300

    adicionando código ao pratica-10.js da tarefa da semana-4

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-10.js
@@ -0,0 +1,75 @@
+let listaSaldo = [{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+},
+{
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+},
+{
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+},
+{
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+},
+{
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+},
+{
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+},
+{
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+}];
+  
+  function exibirTotalRetirada(listaSaldo){
+    let somaRetiradas = 0;
+
+    for(let i=0; i < listaSaldo.length; i++){
+        if(listaSaldo[i].movimentacao === 'retirada'){
+            somaRetiradas += listaSaldo[i].valor;
+        }
+    }
+    return somaRetiradas;
+  }
+
+  function exibirTotalDepositos(listaSaldo){
+    let somaDepositos = 0;
+
+    for(let i=0; i < listaSaldo.length; i++){
+        if(listaSaldo[i].movimentacao === 'deposito'){
+            somaDepositos += listaSaldo[i].valor;
+        }
+    }
+    return somaDepositos;
+  }
+
+  function exibirSaldoFinal(listaSaldo){
+
+     let saldoFinal = 0;
+
+     return  saldoFinal = exibirTotalDepositos(listaSaldo) - exibirTotalRetirada(listaSaldo);
+  }
+
+  function exibirMensagem(listaSaldo){
+
+    let mensagem = exibirSaldoFinal(listaSaldo) > 0 ? console.log('O saldo final da sua conta foi positivo no valor de ' +exibirSaldoFinal(listaSaldo)) : console.log('O saldo final da sua conta foi negativo no valor de ' + exibirSaldoFinal(listaSaldo));
+
+    return mensagem;
+  }
+
+  exibirMensagem(listaSaldo);
+
+  
