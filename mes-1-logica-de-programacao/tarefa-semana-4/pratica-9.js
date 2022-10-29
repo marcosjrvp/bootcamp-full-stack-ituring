8d4bac2fe31bdd57b08acceb5890012e67e9751e
Marcos Junior <marcosjr_rj@hotmail.com>
2022-10-29 20:11:16 -0300

    alterando o código pratica-9.js da tarefa da semana-4

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-9.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-9.js
@@ -1,36 +1,36 @@
 let filmes = [{
-    titulo: "Um Sonho de Liberdade",
-    notaAvaliacao: 9.2,
-    duracao: 142,
-    anoPublicacao: 1994,
-    categoria: "Drama"
-  },
-  {
-    titulo: "Clube da Luta",
-    notaAvaliacao: 8.7,
-    duracao: 139,
-    anoPublicacao: 1999,
-    categoria: "Drama"
-  },
-  {
-    titulo: "Toy Story 3",
-    notaAvaliacao: 8.3,
-    duracao: 103,
-    anoPublicacao: 2010,
-    categoria: "Animação"
-  }];
+  titulo: "Um Sonho de Liberdade",
+  notaAvaliacao: 9.2,
+  duracao: 142,
+  anoPublicacao: 1994,
+  categoria: "Drama"
+},
+{
+  titulo: "Clube da Luta",
+  notaAvaliacao: 8.7,
+  duracao: 139,
+  anoPublicacao: 1999,
+  categoria: "Drama"
+},
+{
+  titulo: "Toy Story 3",
+  notaAvaliacao: 8.3,
+  duracao: 103,
+  anoPublicacao: 2010,
+  categoria: "Animação"
+}];
 
-  function ordenarFilmes(filmes){
+function filmesOrdenados(filmes){
 
-    let filmesOrdenados = filmes.sort((a, b) =>{
-        return a.notaAvaliacao - b.notaAvaliacao; 
-        
-    });
+  const listaFilmesOrdenados = filmes.sort( (a,b)=> a.notaAvaliacao - b.notaAvaliacao);
+  return listaFilmesOrdenados;
+}
 
-    let ultimo = filmesOrdenados.length-1;
+function exibirResultado(filmes){
 
-    console.log(`O filme de melhor avaliação com nota ${filmesOrdenados[ultimo].notaAvaliacao} é : ${filmesOrdenados[ultimo].titulo}`);
-    
-  }
+  let melhorFilme = filmesOrdenados(filmes).pop();
 
-  ordenarFilmes(filmes); 
+  console.log(`O filme de melhor avaliação com nota ${melhorFilme.notaAvaliacao} é : ${melhorFilme.titulo}`);
+}
+
+exibirResultado(filmes);
